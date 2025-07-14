30719b243c276efdeed71c46d710fb905c1de69f
/ rl. Js+Html <crazyzaxjunior@gmail.com>
2025-07-14 18:03:26 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,5 +3,5 @@ const GITHUB_CONFIG = {
   repo: 'PUSH-CH-FAREL',
   path: 'listidch.json', // atau nama file json kamu
   branch: 'main',
-  token: 'ghp_XXXXXXXXXXXXXXXXXXXXXXXXXXXX' // ganti dengan token asli
+  token: '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' // ganti dengan token asli
 };
