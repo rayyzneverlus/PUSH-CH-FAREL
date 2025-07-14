7b2435dc05ec6a2baaf099ffcb7cf137a920c045
/ rl. Js+Html <crazyzaxjunior@gmail.com>
2025-07-14 18:25:06 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,7 +1,7 @@
 const GITHUB_CONFIG = {
   username: 'rayyzneverlus',
   repo: 'PUSH-CH-FAREL',
-  path: 'listidch.json', // atau nama file json kamu
+  path: 'data.json', // atau nama file json kamu
   branch: 'main',
   token: '' // ganti dengan token asli
 };
