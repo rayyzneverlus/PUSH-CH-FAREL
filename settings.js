4089f3928c467911a5e17a0fc239a9b59909077b
/ rl. Js+Html <crazyzaxjunior@gmail.com>
2025-07-14 17:58:56 +0700

    Create settings.js

---
+++ settings.js
@@ -0,0 +1,7 @@
+const GITHUB_CONFIG = {
+  username: 'rayyzneverlus',
+  repo: 'PUSH-CH-FAREL',
+  path: 'listidch.json', // atau nama file json kamu
+  branch: 'main',
+  token: 'ghp_XXXXXXXXXXXXXXXXXXXXXXXXXXXX' // ganti dengan token asli
+};
