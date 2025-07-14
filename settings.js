2abd76d288487423ebcda6190659096101391297
/ rl. Js+Html <crazyzaxjunior@gmail.com>
2025-07-14 18:49:40 +0700

    Delete settings.js

--- settings.js
+++
@@ -1,7 +0,0 @@
-const GITHUB_CONFIG = {
-  username: 'rayyzneverlus',
-  repo: 'PUSH-CH-FAREL',
-  path: 'data.json', // atau nama file json kamu
-  branch: 'main',
-  token: '' // ganti dengan token asli
-};
